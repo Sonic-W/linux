28f2cd785e22f010305d690c33860cda8f11a28e
Sonic-W <2940328256@qq.com>
2020-06-01 16:20:08 +0800

    the one0

--- MysqlStudent/src/com/ysu/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.ysu.dao;
-
-import com.ysu.entity.Product;
-
-public class ProductDao extends BaseDao<Product> {
-	
-}
--- MysqlStudent/src/com/ysu/dao/StudentDao.java
+++
@@ -1,6 +0,0 @@
-package com.ysu.dao;
-import com.ysu.entity.Student;
-
-public class StudentDao extends BaseDao<Student> {
-	
-}
